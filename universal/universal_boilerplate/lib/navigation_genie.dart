ca872bcbb58aac566bb2e56dade403489bc57ca3
ilCONDORA <125373443+ilCONDORA@users.noreply.github.com>
2025-03-14 15:07:34 +0100

    refactor: update navigation properties and add new widget for device navigation

--- universal/universal_boilerplate/lib/navigation_genie.dart
+++ universal/universal_boilerplate/lib/navigation_genie.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:pie_menu/pie_menu.dart';
 
 class NavigationProperties {
@@ -14,7 +13,7 @@ class NavigationProperties {
   final String Function(BuildContext) label;
   final bool showLabel;
   final bool showNavigationWidget;
-  final List<RouteBase> subRoutes;
+  final List<NavigationProperties> subRoutes;
 
   NavigationProperties({
     required this.path,
@@ -69,7 +68,13 @@ List<NavigationProperties> listOfNavigationProperties = [
     path: '/devices',
     nameOfRoute: 'devices',
     screen: Center(
-      child: Text('Devices'),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          NewWidget(),
+          Text('Devices'),
+        ],
+      ),
     ),
     unselectedIcon: Icons.devices_other_outlined,
     selectedIcon: Icons.devices_other,
@@ -77,64 +82,37 @@ List<NavigationProperties> listOfNavigationProperties = [
     label: (context) => 'Devices!! Trans',
     showLabel: true,
     showNavigationWidget: true,
-    subRoutes: [],
+    subRoutes: [
+      NavigationProperties(
+        path: 'device1',
+        nameOfRoute: 'device1',
+        screen: Center(
+          child: Text('Device 1'),
+        ),
+        unselectedIcon: Icons.devices_other_outlined,
+        selectedIcon: Icons.devices_other,
+        showIcons: true,
+        label: (context) => 'Device 1 !! Trans',
+        showLabel: true,
+        showNavigationWidget: true,
+        subRoutes: [],
+      ),
+    ],
   ),
 ];
 
-/* List navigationItemsGenerator({
-  required NavigationTypes navigationType,
-  required BuildContext context,
-}) {
-  switch (navigationType) {
-    case NavigationTypes.bottom:
-      List<BottomNavigationBarItem> navigationItems = [];
-      for (NavigationProperties singleNavProps in listOfNavigationProperties) {
-        if (singleNavProps.showNavigationWidget) {
-          navigationItems.add(
-            BottomNavigationBarItem(
-              icon: singleNavProps.unselectedIcon,
-              activeIcon:
-                  singleNavProps.showIcons ? singleNavProps.selectedIcon : null,
-              label: singleNavProps.showLabel
-                  ? singleNavProps.label(context)
-                  : null,
-            ),
-          );
-        }
-      }
-      return navigationItems;
-    case NavigationTypes.rail:
-      List<NavigationRailDestination> navigationItems = [];
-      for (NavigationProperties singleNavProps in listOfNavigationProperties) {
-        if (singleNavProps.showNavigationWidget) {
-          navigationItems.add(
-            NavigationRailDestination(
-              icon: singleNavProps.unselectedIcon,
-              selectedIcon:
-                  singleNavProps.showIcons ? singleNavProps.selectedIcon : null,
-              label: Text(singleNavProps.label(context)),
-            ),
-          );
-        }
-      }
-      return navigationItems;
-    case NavigationTypes.drawer:
-      List<NavigationDrawerDestination> navigationItems = [];
-      for (NavigationProperties singleNavProps in listOfNavigationProperties) {
-        if (singleNavProps.showNavigationWidget) {
-          navigationItems.add(
-            NavigationDrawerDestination(
-              icon: singleNavProps.unselectedIcon,
-              selectedIcon:
-                  singleNavProps.showIcons ? singleNavProps.selectedIcon : null,
-              label: Text(singleNavProps.label(context)),
-            ),
-          );
-        }
-      }
-      return navigationItems;
+class NewWidget extends StatelessWidget {
+  const NewWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: () => GoRouter.of(context).goNamed('device1'),
+        child: Text('Device 1'));
   }
-} */
+}
 
 GoRouter branchesGenerator() => GoRouter(
       routes: [
@@ -148,7 +126,7 @@ GoRouter branchesGenerator() => GoRouter(
                   path: singleNavProps.path,
                   name: singleNavProps.nameOfRoute,
                   builder: (context, state) => singleNavProps.screen,
-                  routes: singleNavProps.subRoutes,
+                  routes: _generateSubRoutes(singleNavProps.subRoutes),
                 ),
               ],
             );
@@ -156,6 +134,19 @@ GoRouter branchesGenerator() => GoRouter(
         ),
       ],
     );
+
+// Funzione helper per generare ricorsivamente le subroute
+List<RouteBase> _generateSubRoutes(List<NavigationProperties> subRoutes) {
+  return subRoutes.map((subRoute) {
+    return GoRoute(
+      path: subRoute.path,
+      name: subRoute.nameOfRoute,
+      builder: (context, state) => subRoute.screen,
+      // Chiamata ricorsiva per gestire le subroute annidate
+      routes: _generateSubRoutes(subRoute.subRoutes),
+    );
+  }).toList();
+}
 
 class LayoutDispatcher extends StatelessWidget {
   const LayoutDispatcher({super.key, required this.navigationShell});
@@ -200,99 +191,75 @@ class LayoutDispatcher extends StatelessWidget {
       ),
       child: Scaffold(
         key: scaffoldKey,
-        body: SizedBox.expand(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              SelectionArea(child: navigationShell),
-              GNav(
-                mainAxisAlignment: MainAxisAlignment
-                    .spaceAround, //TODO: Ho modificato GButton, capire come funziona l'animazione e se è snappi anche su un'app vuota.
-                rippleColor: Colors.grey[300]!,
-                hoverColor: Colors.grey[100]!,
-                gap: 8,
-                activeColor: Colors.black,
-                iconSize: 24,
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-                duration: Duration(milliseconds: 400),
-                tabBackgroundColor: Colors.grey[100]!,
-                color: Colors.black,
-                tabs: [
-                  for (NavigationProperties singleNavProps
-                      in listOfNavigationProperties)
-                    GButton(
-                      isVisible: singleNavProps.showNavigationWidget,
-                      icon: singleNavProps.unselectedIcon,
-                      text: singleNavProps.label(context),
-                    )
-                ],
-                selectedIndex: navigationShell.currentIndex,
-                onTabChange: (index) => _goToBranch(index),
-              ),
-              Container(
-                height: 80,
-                color: Colors.white,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children:
-                      listOfNavigationProperties.asMap().entries.map((entry) {
-                    final index = entry.key;
-                    final singleNavProps = entry.value;
-                    final isSelected = navigationShell.currentIndex == index;
+        body: Column(
+          children: [
+            Expanded(
+              child: SelectionArea(child: navigationShell),
+            ),
+            Container(
+              height: 80,
+              color: Colors.white,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children:
+                    listOfNavigationProperties.asMap().entries.map((entry) {
+                  final index = entry.key;
+                  final singleNavProps = entry.value;
+                  final isSelected = navigationShell.currentIndex == index;
 
-                    return Visibility(
-                      visible: singleNavProps.showNavigationWidget,
-                      child: MouseRegion(
-                        cursor: SystemMouseCursors.click,
-                        child: GestureDetector(
-                          onTap: () => _goToBranch(index),
-                          child: AnimatedContainer(
-                            duration: Duration(milliseconds: 500),
-                            curve: Curves.easeOutExpo,
-                            decoration: BoxDecoration(
-                              border: isSelected ? Border.all() : null,
-                              borderRadius: BorderRadius.circular(18),
-                            ),
-                            padding: EdgeInsets.all(10),
-                            child: Wrap(
-                              spacing: arrangementOfIconAndLabel == Axis.horizontal ? 5 : 0,
-                              crossAxisAlignment: WrapCrossAlignment.center,
-                              direction: arrangementOfIconAndLabel,
-                              children: [
-                                Visibility(
-                                  visible: singleNavProps.showIcons,
-                                  child: Icon(
-                                    isSelected &&
-                                            singleNavProps.selectedIcon != null
-                                        ? singleNavProps.selectedIcon
-                                        : singleNavProps.unselectedIcon,
+                  return Visibility(
+                    visible: singleNavProps.showNavigationWidget,
+                    child: MouseRegion(
+                      cursor: SystemMouseCursors.click,
+                      child: GestureDetector(
+                        onTap: () => _goToBranch(index),
+                        child: AnimatedContainer(
+                          duration: Duration(milliseconds: 500),
+                          curve: Curves.easeOutExpo,
+                          decoration: BoxDecoration(
+                            border: isSelected ? Border.all() : null,
+                            borderRadius: BorderRadius.circular(18),
+                          ),
+                          padding: EdgeInsets.all(10),
+                          child: Wrap(
+                            spacing:
+                                arrangementOfIconAndLabel == Axis.horizontal
+                                    ? 5
+                                    : 0,
+                            crossAxisAlignment: WrapCrossAlignment.center,
+                            direction: arrangementOfIconAndLabel,
+                            children: [
+                              Visibility(
+                                visible: singleNavProps.showIcons,
+                                child: Icon(
+                                  isSelected &&
+                                          singleNavProps.selectedIcon != null
+                                      ? singleNavProps.selectedIcon
+                                      : singleNavProps.unselectedIcon,
+                                  color:
+                                      isSelected ? Colors.black : Colors.grey,
+                                ),
+                              ),
+                              Visibility(
+                                visible: singleNavProps.showLabel && isSelected,
+                                child: Text(
+                                  singleNavProps.label(context),
+                                  style: TextStyle(
                                     color:
                                         isSelected ? Colors.black : Colors.grey,
                                   ),
                                 ),
-                                Visibility(
-                                  visible:
-                                      singleNavProps.showLabel && isSelected,
-                                  child: Text(
-                                    singleNavProps.label(context),
-                                    style: TextStyle(
-                                      color: isSelected
-                                          ? Colors.black
-                                          : Colors.grey,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
+                              ),
+                            ],
                           ),
                         ),
                       ),
-                    );
-                  }).toList(),
-                ),
-              )
-            ],
-          ),
+                    ),
+                  );
+                }).toList(),
+              ),
+            )
+          ],
         ),
       ),
     );
