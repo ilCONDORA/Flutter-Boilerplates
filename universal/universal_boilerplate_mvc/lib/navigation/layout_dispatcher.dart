5cd2226a61342942f029a670466fc0fabed8893f
Andrea Condorelli <>
2025-07-02 17:38:53 +0200

    Removed import of Gnav

--- universal/universal_boilerplate_mvc/lib/navigation/layout_dispatcher.dart
+++ universal/universal_boilerplate_mvc/lib/navigation/layout_dispatcher.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
 
 import '../components/navigation_buttons.dart';
 
