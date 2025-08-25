cc41325f31af79d390d71ab5dfe8331483bf3200
ilCONDORA <125373443+ilCONDORA@users.noreply.github.com>
2025-08-25 14:14:30 +0200

    Resolved problem size when generating the list

--- universal/universal_boilerplate_mvc/lib/screens/active/tasks_page.dart
+++ universal/universal_boilerplate_mvc/lib/screens/active/tasks_page.dart
@@ -7,33 +7,42 @@ import '../../scrollable_page_widget.dart';
 class TasksPage extends ScrollablePageWidget {
   const TasksPage({super.key});
 
-
   @override
   Widget buildPageContent(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
       spacing: 12,
       children: [
         Center(
           child: Text('Active Tasks Screen', style: TextStyle(fontSize: 24)),
         ),
-
-        for (int i = 1; i <= 20; i++)
-          SizedBox(
-            width: 400,
-            child: ListTile(
-              title: Text('Task $i'),
-              subtitle: Text('Description of task with id $i'),
-              trailing: IconButton(
-                icon: Icon(Icons.arrow_forward),
-                onPressed:
-                    () => GoRouter.of(context).goNamed(
-                      DefinedRoutesNames.taskDetails,
-                      pathParameters: {'id': i.toString()},
+        Flexible(
+          child: ListView.builder(
+            itemCount: 20,
+            shrinkWrap: true,
+            itemBuilder: (context, index) {
+              final taskId = index + 1;
+              return Container(
+                margin: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+                child: Card(
+                  child: ListTile(
+                    title: Text('Task $taskId'),
+                    subtitle: Text('Description of task with id $taskId'),
+                    trailing: IconButton(
+                      icon: Icon(Icons.arrow_forward),
+                      onPressed:
+                          () => GoRouter.of(context).goNamed(
+                            DefinedRoutesNames.taskDetails,
+                            pathParameters: {'id': taskId.toString()},
+                          ),
                     ),
-              ),
-            ),
+                  ),
+                ),
+              );
+            },
           ),
+        ),
       ],
     );
   }
